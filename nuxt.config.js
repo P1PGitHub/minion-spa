bb0c49bcfa47849ceb304b028f83da74ab2e5280
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-03 18:48:12 -0400

    installed vue-moment and added to nuxt as a plugin.

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,10 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [{ src: '@/plugins/vcalendar.js', ssr: false }],
+  plugins: [
+    { src: '@/plugins/vcalendar.js', ssr: false },
+    { src: '@/plugins/vue-moment.js', ssr: false },
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
