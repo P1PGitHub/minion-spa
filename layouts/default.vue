0d6049d156cfacc22fae1b931df24d3f04a9f042
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-03 18:51:54 -0400

    feat: added page loading component to default layout.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="min-h-screen bg-gray-100" :class="{ 'overflow-y-hidden': scrollLock }">
+  <div
+    class="min-h-screen bg-gray-100"
+    :class="{ 'overflow-y-hidden': scrollLock }"
+  >
     <Navbar />
     <Modal />
     <Toast />
+    <PageLoading />
     <Nuxt class="pt-22" />
   </div>
 </template>
 
 <script>
-import Modal from "@/components/ui/modal"
-import Toast from "@/components/ui/toast"
+import Modal from '@/components/ui/modal'
+import PageLoading from '@/components/ui/pageLoading'
+import Toast from '@/components/ui/toast'
 export default {
-  name: "DefualtLayout",
+  name: 'DefualtLayout',
   components: {
     Modal,
-    Toast
+    PageLoading,
+    Toast,
   },
   data() {
     return {
-      scrollLock: false
+      scrollLock: false,
     }
   },
   created() {
-    this.$root.$on("showModal", () => {
+    this.$root.$on('showModal', () => {
       this.scrollLock = true
     }),
-    this.$root.$on("modalClose", () => {
-      this.scrollLock = false
-    })
-  }
+      this.$root.$on('modalClose', () => {
+        this.scrollLock = false
+      })
+  },
 }
 </script>
