e52561a7cf36462b48ad37a4010e9b4c43d06406
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-14 20:32:04 -0400

    feat: added csqr drafts list page.

---
+++ pages/reports/csqr/drafts.vue
@@ -0,0 +1,98 @@
+<template>
+  <PageBody>
+    <template v-slot:page-header>
+      <div class="flex items-center space-x-4">
+        <h1 class="text-xl">Team CSQR Reports</h1>
+        <Loading v-if="isLoading" />
+      </div>
+      <ButtonLink fontSize="sm" :link="{ name: 'reports-csqr' }" theme="hollow"
+        >Back</ButtonLink
+      >
+    </template>
+    <template v-slot:page-content>
+      <div>
+        <ReportListItem
+          v-for="(report, index) in reports"
+          :key="report.id"
+          :report="report"
+          :editLink="true"
+          :border="index !== reports.length - 1"
+        />
+      </div>
+      <div class="flex items-center justify-end space-x-2">
+        <ActionButton theme="hollow" spacing="sm" @click="getPrevious"
+          >&lt;</ActionButton
+        >
+        <ActionButton theme="primary" spacing="sm"
+          >{{ currentPage }} / {{ maxPage }}</ActionButton
+        >
+        <ActionButton theme="hollow" spacing="sm" @click="getNext"
+          >&gt;</ActionButton
+        >
+      </div>
+    </template>
+  </PageBody>
+</template>
+
+<script>
+import ActionButton from '@/components/ui/actionButton'
+import ButtonLink from '@/components/ui/buttonLink'
+import Loading from '@/components/common/Loading'
+import PageBody from '@/components/ui/pageBody'
+import ReportListItem from '@/components/ui/reportListItem'
+export default {
+  name: 'CSQRList',
+  components: {
+    ActionButton,
+    ButtonLink,
+    Loading,
+    PageBody,
+    ReportListItem,
+  },
+  data() {
+    return {
+      isLoading: true,
+      reports: [],
+      currentPage: 1,
+      maxPage: 1,
+    }
+  },
+  methods: {
+    async getNext() {
+      if (this.currentPage < this.maxPage) {
+        this.isLoading = true
+        let reportResponse = await this.$store.dispatch(
+          'api/get',
+          `/reports/customer_service/drafts/?page=${this.currentPage + 1}/`
+        )
+        this.reports = reportResponse.results
+        this.currentPage++
+        this.maxPage = Math.ceil(reportResponse.count / 25)
+        this.isLoading = false
+      }
+    },
+    async getPrevious() {
+      if (this.currentPage > 1) {
+        this.isLoading = true
+        let reportResponse = await this.$store.dispatch(
+          'api/get',
+          `/reports/customer_service/drafts/?page=${this.currentPage - 1}/`
+        )
+        this.reports = reportResponse.results
+        this.currentPage--
+        this.maxPage = Math.ceil(reportResponse.count / 25)
+        this.isLoading = false
+      }
+    },
+  },
+  async created() {
+    let reportResponse = await this.$store.dispatch(
+      'api/get',
+      '/reports/customer_service/drafts/'
+    )
+    this.maxPage = Math.ceil(reportResponse.count / 25)
+    this.reports = reportResponse.results
+    this.isLoading = false
+  },
+}
+</script>
