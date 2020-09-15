da8fc73c3988383faa6b3f18ed4b380178c94aa0
fishink-python <31522303+fishink-python@users.noreply.github.com>
2020-09-14 20:32:38 -0400

    feat: added action button ui component.

---
+++ components/ui/actionButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    class="flex items-center justify-center font-bold rounded"
+    :class="{
+      'text-sm': fontSize == 'sm',
+      'text-md': fontSize == 'md',
+      'text-lg': fontSize == 'lg',
+      'space-x-0 px-2 py-1': spacing == 'none',
+      'space-x-2 px-2 py-1': spacing == 'sm',
+      'space-x-4 px-4 py-2': spacing == 'md',
+      'space-x-8 px-8 py-4': spacing == 'lg',
+      'bg-blue-600 text-white border border-blue-600 hover:bg-blue-700 hover:border-blue-700':
+        theme == 'primary',
+      'bg-white text-teal-700 border border-teal-700 hover:bg-teal-200':
+        theme == 'secondary',
+      'text-gray-800 border border-gray-800 hover:bg-gray-200':
+        theme == 'hollow',
+      'text-white border border-white hover:bg-gray-400 hover:bg-opacity-25':
+        theme == 'hollow-light',
+    }"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'buttonLink',
+  props: {
+    fontSize: {
+      type: String,
+      required: false,
+      default: 'md',
+    },
+    spacing: {
+      type: String,
+      required: false,
+      default: 'md',
+    },
+    theme: {
+      type: String,
+      required: false,
+      default: 'primary',
+    },
+  },
+}
+</script>
