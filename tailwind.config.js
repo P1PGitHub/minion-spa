df5eda20a64595184fd89cf68d8eff4202a64bde
Devin Sharpe <devinsharpe@Devins-MacBook-Pro.local>
2020-10-08 12:08:59 -0400

    feat: enable tailwind dark mode experimental.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  experimental: {
+    darkModeVariant: true,
+  },
+  dark: 'class',
   theme: {
     fontFamily: {
       sans: ['Red Hat Display', 'Helvetica', 'Arial', 'sans-serif'],
