82797495639079006ac812433bd05b04b37747f7
Devin Sharpe <devinrsharpe@gmail.com>
2020-11-12 17:33:18 -0500

    feat: added simple footer to all application pages.

--- components/ui/pageBody.vue
+++ components/ui/pageBody.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col items-center px-4">
-    <section
-      class="flex items-center justify-between w-full pb-4 px-2 border-b border-dashed border-gray-400 text-gray-800 dark:text-white"
+  <div class="flex flex-col items-center min-h-screen">
+    <header
+      class="flex items-center justify-between w-full pb-4 px-4 px-2 border-b border-dashed border-gray-400 text-gray-800 dark:text-white"
     >
       <slot name="page-header"> </slot>
-    </section>
+    </header>
     <main
-      class="px-2 py-4 md:px-4 text-gray-800 dark:text-white container"
+      class="px-6 py-4 md:px-8 text-gray-800 dark:text-white container h-full"
       :class="{
         'space-y-0': spacing == 'none',
         'space-y-2': spacing == 'sm',
@@ -16,6 +16,23 @@
     >
       <slot name="page-content"></slot>
     </main>
+    <footer
+      class="w-full mt-auto bg-gray-200 mt-4 p-4 text-gray-700 space-y-1 text-sm"
+    >
+      <h3 class="text-base font-semibold">Minion</h3>
+      <p>
+        Please report any bugs or features requests to
+        <a href="mailto:devin.s@priority1pos.com" class="underline"
+          >devin.s@priority1pos.com</a
+        >
+      </p>
+      <div class="flex flex-wrap items-center space-x-4">
+        <nuxt-link to="/" class="underline">Home</nuxt-link>
+        <nuxt-link :to="{ name: 'settings' }" class="underline"
+          >Settings</nuxt-link
+        >
+      </div>
+    </footer>
   </div>
 </template>
 
