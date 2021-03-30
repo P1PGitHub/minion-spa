52e2f942ed5af2c35f7daa63e79f65c76f1f3879
Devin Sharpe <devinrsharpe@gmail.com>
2021-03-30 11:27:30 -0500

    feat: redesigned page loading animation and screen.

--- components/ui/pageLoading.vue
+++ components/ui/pageLoading.vue
@@ -1,10 +1,11 @@
 <template>
   <div
-    class="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50 z-50"
+    class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-25 z-50"
     :class="{ block: show, hidden: !show }"
   >
-    <div class="space-y-4">
-      <h1 class="text-xl font-bold text-blue-700">Bello!</h1>
+    <div
+      class="space-y-4 bg-white dark:bg-gray-800 flex flex-col items-center justify-center rounded shadow-lg w-32 h-32 border-2 border-blue-400"
+    >
       <img
         src="@/assets/svg/other/spinner-two-theme.svg"
         alt="Loading Icon"
