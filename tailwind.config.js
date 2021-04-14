9729ba7b6121240aa4aadae4f09f83f08487aec9
Devin Sharpe <devinrsharpe@gmail.com>
2021-04-14 13:26:30 -0400

    dev: tailwind update and config changes for 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,40 +4,45 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
+const colors = require('tailwindcss/colors')
 module.exports = {
-  experimental: {
-    darkModeVariant: true,
-  },
-  dark: 'class',
+  darkMode: 'class',
   theme: {
     fontFamily: {
       sans: ['Red Hat Display', 'Helvetica', 'Arial', 'sans-serif'],
     },
     extend: {
+      colors: {
+        blue: colors.lightBlue,
+        gray: colors.blueGray,
+        green: colors.green,
+        orange: colors.amber,
+        teal: colors.teal,
+      },
       maxHeight: {
-        '128': '32rem',
+        128: '32rem',
       },
       maxWidth: {
-        '64': '16rem',
-        '96': '24rem',
-        '128': '32rem',
+        64: '16rem',
+        96: '24rem',
+        128: '32rem',
       },
       minWidth: {
-        '64': '16rem',
-        '96': '24rem',
+        64: '16rem',
+        96: '24rem',
       },
       spacing: {
         '-36': '-9rem;',
         '-34': '-8.5rem',
-        '14': '3.5rem',
-        '22': '5.5rem',
-        '96': '24rem',
-        '128': '32rem',
+        14: '3.5rem',
+        22: '5.5rem',
+        96: '24rem',
+        128: '32rem',
       },
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/custom-forms')],
+  plugins: [require('@tailwindcss/forms')({ strategy: 'class' })],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
